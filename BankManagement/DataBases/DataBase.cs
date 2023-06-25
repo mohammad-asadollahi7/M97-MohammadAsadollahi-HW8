921a415b9350303446c61e6bc7283ff3dd18818d
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-06-25 19:31:28 +0430

    Add some data to database

--- BankManagement/DataBases/DataBase.cs
+++ BankManagement/DataBases/DataBase.cs
@@ -1,6 +1,17 @@
-﻿namespace BankManagement.DataBases
+﻿using BankManagement.Models;
+
+namespace BankManagement.DataBases
 {
     public class DataBase
     {
+        public static List<User> users = new List<User>()
+        {
+            new User(){Name="ali", Family="hassani", NationalCode="123456781",
+                       PhoneNumber="09111234567"},
+            new User(){Name="elham", Family="ebrahimi", NationalCode="123456782",
+                       PhoneNumber="09121234567"},
+            new User(){Name="abbas", Family="karimi", NationalCode="123456783",
+                       PhoneNumber="09131234567"}
+        };
     }
 }
