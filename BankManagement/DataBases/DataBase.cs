f7b465a37cb3ae13410b538befea48a3bef73f06
M.Asadollahi <asadolahi.ma7@gmail.com>
2023-06-25 23:00:11 +0430

    Add turnover data to database

--- BankManagement/DataBases/DataBase.cs
+++ BankManagement/DataBases/DataBase.cs
@@ -13,4 +13,37 @@ public static class DataBase
         new User(){Name="abbas", Family="karimi", NationalCode="1234567893",
                    PhoneNumber="09131234567"}
     };
+
+    public static List<Turnover> turnovers = new List<Turnover>()
+    {
+        new Turnover()
+        {
+            Id=1,
+            NationalCode="1234567891",
+            Debit=1000000,
+            TransactionDate=Convert.ToDateTime("1389-02-03"),
+            Description="خرید از کارت خوان",
+            Balance=12000000
+        },
+
+        new Turnover()
+        {
+            Id=2,
+            NationalCode="1234567891",
+            Credit=2000000,
+            TransactionDate=Convert.ToDateTime("1389-05-06"),
+            Description="نقدی",
+            Balance=14000000
+        },
+
+        new Turnover()
+        {
+            Id=1,
+            NationalCode="1234567891",
+            Credit=1500000,
+            TransactionDate=Convert.ToDateTime("1389-03-09"),
+            Description="چک نقدی",
+            Balance=15500000
+        }
+    };
 }
--- BankManagement/Models/Turnover.cs
+++ BankManagement/Models/Turnover.cs
@@ -1,6 +1,15 @@
-﻿namespace BankManagement.Models
+﻿using System.Security.Principal;
+
+namespace BankManagement.Models
 {
     public class Turnover
     {
+        public int Id { get; set; }
+        public string NationalCode { get; set; }
+        public long? Debit { get; set; }
+        public long? Credit { get; set; }
+        public DateTime  TransactionDate { get; set; }
+        public string Description { get; set; }
+        public long  Balance { get; set; }
     }
 }
